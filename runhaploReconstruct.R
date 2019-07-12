79e8a90ad712bf9db8607890c4ac9ea59b993562
Juanita Diaz <jdiaz22@wisc.edu>
2019-07-12 11:01:31 -0500

    get list of scaffolds for reconstruct_hb from filtered data set instead of from initial sync file

--- runhaploReconstruct.R
+++ runhaploReconstruct.R
@@ -14,7 +14,8 @@ data_filtered=initialize_SNP_time_series(chr=data$chr, pos=data$pos, base.freq=d
 ## Reconstruct haplotype-blocks
 #default values used for ***put in list of parameters***
 #create list of unique scaffold values, then reconstruct haplotype blocks on each unique scaffold
-Scaffolds <- unique(data$chr)
+Scaff_in_Filt <- data_filtered@col.info
+Scaffolds <- unique(data_filtered$chr)
 Scaffolds <- as.character(Scaffolds)
 data_reconst <- lapply(Scaffolds, function(x) reconstruct_hb(data_filtered, chrom=x))
 
